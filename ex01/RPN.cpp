6df11c911608ff54625d2602942af085e4bea43c
mihowk <elhaia.mohamed555@gmail.com>
2026-08-03 20:09:35 +0100

    just finished ex01 but needs a bit of testing

---
+++ ex01/RPN.cpp
@@ -0,0 +1,60 @@
+#include "RPN.hpp"
+
+void calculat_RPN(const std::string &expression)
+{
+    std::stack<int> stack;
+
+    for (size_t i = 0; i < expression.size(); i++)
+    {
+        char c = expression[i];
+
+        if (c == ' ')
+            continue;
+        
+        if (isdigit(c))
+            stack.push(c - '0');
+
+        else if (c == '+' || c == '-' || c == '*' || c == '/')
+        {
+            if (stack.size() < 2)
+            {
+                std::cerr << "Error" << std::endl;
+                return;
+            }
+
+            int b = stack.top();
+            stack.pop();
+            int a = stack.top();
+            stack.pop();
+
+            if (c == '+')
+                stack.push(a + b);
+            else if (c == '-')
+                stack.push(a - b);
+            else if (c == '*')
+                stack.push(a * b);
+            else if (c == '/')
+            {
+                if (b == 0)
+                {
+                    std::cerr << "Error" << std::endl;
+                    return;
+                }
+                stack.push(a / b);
+            }
+        }
+        else
+        {
+            std::cerr << "Error" << std::endl; 
+            return;
+        }
+    }
+
+    if (stack.size() != 1)
+    {
+        std::cerr << "Error" << std::endl;
+        return;
+    }
+
+    std::cout << stack.top() << std::endl;
+}
---
+++ ex01/RPN.hpp
@@ -0,0 +1,10 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <sstream>
+
+void calculat_RPN(const std::string &expression);
+
+#endif
